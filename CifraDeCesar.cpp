0e001419806b4a9d744c39ed146b34f8f575ae96
Lucas Santos Rodrigues <84406944+santoslucas@users.noreply.github.com>
2022-09-12 09:37:26 -0300

    Add files via upload

---
+++ CifraDeCesar.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+int main (void){
+    int numCasos;
+    cin >> numCasos;
+
+    for(int i=0; i<numCasos; i++){
+        string cifra;
+        char descifra;
+
+        int deslocamento;
+
+        cin >> cifra;
+        cin >> deslocamento;
+
+        int tamanho = cifra.length();
+
+        for(int j=0; j<tamanho; j++){
+            for(int k=0; k<deslocamento; k++){
+                descifra = (cifra[j]-1);
+
+                if(descifra <= 64){
+                    descifra = 90;
+                }
+            }
+            cout << descifra;
+        }
+
+        cout << endl;
+    }
+}
